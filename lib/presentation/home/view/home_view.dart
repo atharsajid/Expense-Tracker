4abe4396581ab5b11d95b0567a26b9d434a271d1
atharsajid <athersajid620@gmail.com>
2025-05-27 23:18:13 +0500

    Created Home View.

---
+++ lib/presentation/home/view/home_view.dart
@@ -0,0 +1,130 @@
+import 'package:expense_tracker/constants/app_images.dart';
+import 'package:expense_tracker/presentation/home/controller/home_view_controller.dart';
+import 'package:expense_tracker/utilities/app_theme.dart';
+import 'package:expense_tracker/widgets/custom_app_bar.dart';
+import 'package:expense_tracker/widgets/custom_floation_action_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  HomeViewController get controller => Get.put(HomeViewController());
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      appBar: const CustomAppBar(
+        isHome: true,
+      ),
+      backgroundColor: AppTheme.baseColor,
+      body: Obx(() {
+        return SizedBox(
+          width: size.width,
+          height: size.height,
+          child: controller.homeTabs[controller.currentTabIndex.value],
+        );
+      }),
+      floatingActionButton: CustomFloatingActionButton(
+        svgIcon: AppImages.addIcon,
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: AppTheme.grey100Color.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 60,
+              offset: const Offset(0, -5),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          child: BottomAppBar(
+            height: 82,
+            clipBehavior: Clip.antiAlias,
+            notchMargin: 10,
+            shape: const CircularNotchedRectangle(),
+            color: AppTheme.lightColor,
+            surfaceTintColor: AppTheme.lightColor,
+            elevation: 12,
+            child: SizedBox(
+              height: 80,
+              width: size.width,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Obx(() {
+                    return IconButton(
+                      onPressed: () {
+                        controller.currentTabIndex.value = 0;
+                      },
+                      icon: buildSvgIconWidget(assetName: "grid_squares", isSelected: controller.currentTabIndex.value == 0, height: 30),
+                    );
+                  }),
+                  const Spacer(),
+                  Obx(() {
+                    return IconButton(
+                      onPressed: () {
+                        controller.currentTabIndex.value = 1;
+                      },
+                      icon: buildSvgIconWidget(assetName: "bar_chart_square", isSelected: controller.currentTabIndex.value == 1),
+                    );
+                  }),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSvgIconWidget({required String assetName, EdgeInsetsGeometry? padding, double? width, double? height, bool isSelected = false}) {
+    return Stack(
+      fit: StackFit.loose,
+      alignment: Alignment.bottomCenter,
+      children: [
+        Padding(
+          padding: padding ?? const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+          child: SvgPicture.asset(
+            "assets/icons/$assetName.svg",
+            height: height,
+            width: width,
+            colorFilter: ColorFilter.mode(
+              isSelected ? AppTheme.primaryColor : AppTheme.darkGreyColor,
+              BlendMode.srcIn,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: -10,
+          child: AnimatedContainer(
+            width: isSelected ? 40 : 0,
+            // height: 90,
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            alignment: Alignment.bottomCenter,
+            padding: padding ?? const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            child: const Divider(
+              color: AppTheme.primaryColor,
+              thickness: 2,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
