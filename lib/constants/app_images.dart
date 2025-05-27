21f4a3b29e2f0fd143dc026d5b4607404c9e570a
atharsajid <athersajid620@gmail.com>
2025-05-27 23:12:52 +0500

    Created  Constants.
    Created app images class.
    Created app strings class.

---
+++ lib/constants/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String addIcon = "assets/icons/plus.svg";
+  static const String airplaneIcon = "assets/icons/airplane.png";
+  static const String bagIcon = "assets/icons/bag.png";
+  static const String foodIcon = "assets/icons/food.png";
+  static const String rentIcon = "assets/icons/rent.png";
+  static const String transportIcon = "assets/icons/transport.png";
+  static const String appLogo = "assets/icons/logo.png";
+}
