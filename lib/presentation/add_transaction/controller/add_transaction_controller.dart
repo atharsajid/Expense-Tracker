aa856953c6a5c37b7708c55a5994f7d2200a9f8d
atharsajid <athersajid620@gmail.com>
2025-05-29 00:23:53 +0500

    Implemented add transaction and save in local db.

--- lib/presentation/add_transaction/controller/add_transaction_controller.dart
+++ lib/presentation/add_transaction/controller/add_transaction_controller.dart
@@ -1,11 +1,19 @@
-import 'package:expense_tracker/constants/app_images.dart';
+import 'package:expense_tracker/constants/app_constants.dart';
+import 'package:expense_tracker/constants/app_strings.dart';
+import 'package:expense_tracker/data/repository/category_repository.dart';
+import 'package:expense_tracker/data/repository/transaction_repository.dart';
 import 'package:expense_tracker/domain/enums/transaction_type.dart';
+import 'package:expense_tracker/domain/models/transaction.dart';
 import 'package:expense_tracker/domain/models/transaction_category.dart';
+import 'package:expense_tracker/utilities/app_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class AddTransactionController extends GetxController {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TransactionRepository _transactionRepository = TransactionRepository();
+  final CategoryRepository _categoryRepository = CategoryRepository();
 
   final TextEditingController amountController = TextEditingController();
   final TextEditingController categoryController = TextEditingController();
@@ -18,39 +26,49 @@ class AddTransactionController extends GetxController {
 
   TransactionType? selectedTransactionType;
 
+  DateTime? selectedDate;
+
   @override
   void onInit() {
     super.onInit();
-    addCategories();
+    getAllCategories();
   }
 
-  addCategories() {
-    categoryList.addAll([
-      TransactionCategory(
-        id: "1",
-        name: "Food",
-        icon: AppImages.foodIcon,
-      ),
-      TransactionCategory(
-        id: "2",
-        name: "Transport",
-        icon: AppImages.transportIcon,
-      ),
-      TransactionCategory(
-        id: "3",
-        name: "Salary",
-        icon: AppImages.cashIcon,
-      ),
-      TransactionCategory(
-        id: "4",
-        name: "Rent",
-        icon: AppImages.rentIcon,
-      ),
-      TransactionCategory(
-        id: "5",
-        name: "Shopping",
-        icon: AppImages.bagIcon,
-      ),
-    ]);
+  Future onDateSelect(BuildContext context) async {
+    selectedDate = await Helper.openDatePicker(
+      context,
+      initialDate: selectedDate,
+    );
+    if (selectedDate != null) {
+      dateController.text = DateFormat(AppConstants.dateFormat).format(selectedDate!);
+      update(['date']);
+    }
+  }
+
+  void onAddTransaction() async {
+    try {
+      Map<String, dynamic> data = {
+        "id": DateTime.now().millisecondsSinceEpoch,
+        "date": selectedDate?.toIso8601String(),
+        "category": selectedCategory?.toJson(),
+        "type": selectedTransactionType?.name,
+        "amount": num.tryParse(amountController.text),
+        "description": descriptionController.text
+      };
+      final bool result = await _transactionRepository.addTransaction(data);
+      if (result) {
+        Get.back(result: Transaction.fromJson(data));
+        Helper.showSuccessSnackBar(message: AppStrings.transactionAddedSuccessfullyMsg);
+      } else {
+        Helper.showErrorSnackBar(message: AppStrings.someThingWentWrongMsg);
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  getAllCategories() async {
+    categoryList = await _categoryRepository.getAllCategories();
+    update();
   }
 }
--- lib/presentation/add_transaction/view/add_transaction_view.dart
+++ lib/presentation/add_transaction/view/add_transaction_view.dart
@@ -5,9 +5,11 @@ import 'package:expense_tracker/domain/models/transaction_category.dart';
 import 'package:expense_tracker/presentation/add_transaction/controller/add_transaction_controller.dart';
 import 'package:expense_tracker/utilities/app_theme.dart';
 import 'package:expense_tracker/utilities/extensions.dart/string_extension.dart';
+import 'package:expense_tracker/utilities/size_utils.dart';
 import 'package:expense_tracker/utilities/text_field_validators.dart';
 import 'package:expense_tracker/widgets/custom_app_bar.dart';
 import 'package:expense_tracker/widgets/custom_bottom_sheet.dart';
+import 'package:expense_tracker/widgets/custom_button.dart';
 import 'package:expense_tracker/widgets/custom_image_view.dart';
 import 'package:expense_tracker/widgets/custom_text_field_widget.dart';
 import 'package:flutter/material.dart';
@@ -29,170 +31,186 @@ class AddTransactionView extends StatelessWidget {
         onBackIconPressed: () => Get.back(),
       ),
       backgroundColor: AppTheme.lightColor,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: controller.formKey,
-          child: Column(
-            children: [
-              CustomTextFieldWidget(
-                controller: controller.amountController,
-                upperLabel: "Amount",
-                asterisk: "\u{002A}",
-                hintValue: "Enter Amount".tr,
-                outerPaddingField: EdgeInsets.zero,
-                prefixText: GestureDetector(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 12.0),
-                    child: Row(
-                      children: [
-                        FittedBox(
-                          child: Text(
-                            "USD",
-                            style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: AppTheme.greyColor),
-                            overflow: TextOverflow.fade,
-                            maxLines: 1,
-                          ),
+      body: Form(
+        key: controller.formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            CustomTextFieldWidget(
+              controller: controller.amountController,
+              upperLabel: "Amount",
+              asterisk: "\u{002A}",
+              hintValue: "Enter Amount".tr,
+              outerPaddingField: EdgeInsets.zero,
+              prefixText: GestureDetector(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Row(
+                    children: [
+                      FittedBox(
+                        child: Text(
+                          "USD",
+                          style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: AppTheme.greyColor),
+                          overflow: TextOverflow.fade,
+                          maxLines: 1,
                         ),
-                        const Icon(
-                          Icons.keyboard_arrow_down,
-                          color: AppTheme.greyColor,
-                        ),
-                      ],
+                      ),
+                      const Icon(
+                        Icons.keyboard_arrow_down,
+                        color: AppTheme.greyColor,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              prefixIconConstraints: const BoxConstraints(
+                minWidth: 40,
+                maxWidth: 75,
+              ),
+              showStaticBottomShadow: true,
+              validator: (value) {
+                if (value.isNullOREmpty) {
+                  return "Required";
+                }
+                if (num.tryParse(value ?? "") == null) return "Invalid Amount";
+                return null;
+              },
+              type: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter(RegExp(r'[0-9.]'), allow: true),
+              ],
+            ),
+            CustomTextFieldWidget(
+              controller: controller.categoryController,
+              isEnabled: false,
+              upperLabel: "Category".tr,
+              asterisk: "\u{002A}",
+              hintValue: "Select Category".tr,
+              prefixText: Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: CustomImageView(
+                  svgPath: AppImages.gridSquareSvg,
+                  color: AppTheme.greyColor,
+                ),
+              ),
+              suffixIcon: Icons.keyboard_arrow_down,
+              showStaticBottomShadow: true,
+              validator: (value) => Validators.required(
+                value,
+              ),
+              onTap: () {
+                valueSelectionBottomSheet<TransactionCategory>(
+                  context,
+                  size: size,
+                  headingText: "Select Category",
+                  valueKey: "name",
+                  selectedValue: null,
+                  selectedValueID: controller.selectedCategory?.id ?? "",
+                  sheetHeight: 0.5,
+                  filterList: ValueNotifier(controller.categoryList),
+                  onTap: (Object obj) {
+                    controller.selectedCategory = obj as TransactionCategory?;
+                    controller.categoryController.text = controller.selectedCategory?.name ?? "";
+                    Get.back();
+                  },
+                );
+              },
+            ),
+            CustomTextFieldWidget(
+              controller: controller.typeController,
+              isEnabled: false,
+              upperLabel: "Type".tr,
+              asterisk: "\u{002A}",
+              hintValue: "Select Type".tr,
+              prefixText: Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: CustomImageView(
+                  svgPath: AppImages.transactionTypeSvg,
+                  color: AppTheme.greyColor,
+                ),
+              ),
+              suffixIcon: Icons.keyboard_arrow_down,
+              showStaticBottomShadow: true,
+              validator: (value) => Validators.required(
+                value,
+              ),
+              onTap: () {
+                valueSelectionBottomSheet<TransactionType>(
+                  context,
+                  size: size,
+                  headingText: "Select Type",
+                  valueKey: "name",
+                  selectedValue: controller.selectedTransactionType?.name,
+                  sheetHeight: 0.3,
+                  filterList: ValueNotifier(TransactionType.values),
+                  onTap: (Object obj) {
+                    controller.selectedTransactionType = obj as TransactionType?;
+                    controller.typeController.text = controller.selectedTransactionType?.name ?? "";
+                    Get.back();
+                  },
+                );
+              },
+            ),
+            GetBuilder(
+                init: controller,
+                id: "date",
+                builder: (_) {
+                  return CustomTextFieldWidget(
+                    controller: controller.dateController,
+                    isEnabled: false,
+                    upperLabel: "Select Date".tr,
+                    asterisk: "\u{002A}",
+                    hintValue: "Ex. 01 Jan, 2025".tr,
+                    prefixText: Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: CustomImageView(
+                        svgPath: AppImages.calendarSvg,
+                        color: AppTheme.greyColor,
+                      ),
                     ),
-                  ),
-                ),
-                prefixIconConstraints: const BoxConstraints(
-                  minWidth: 40,
-                  maxWidth: 75,
-                ),
-                showStaticBottomShadow: true,
-                validator: (value) {
-                  if (value.isNullOREmpty) {
-                    return "Required";
-                  }
-                  if (num.tryParse(value ?? "") == null) return "Invalid Amount";
-                  return null;
-                },
-                type: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter(RegExp(r'[0-9.]'), allow: true),
-                ],
-              ),
-              CustomTextFieldWidget(
-                controller: controller.categoryController,
-                isEnabled: false,
-                upperLabel: "Category".tr,
-                asterisk: "\u{002A}",
-                hintValue: "Select Category".tr,
-                prefixText: Padding(
-                  padding: const EdgeInsets.only(left: 12),
-                  child: CustomImageView(
-                    svgPath: AppImages.gridSquareSvg,
-                    color: AppTheme.greyColor,
-                  ),
-                ),
-                suffixIcon: Icons.keyboard_arrow_down,
-                showStaticBottomShadow: true,
-                validator: (value) => Validators.required(
-                  value,
-                ),
-                onTap: () {
-                  valueSelectionBottomSheet<TransactionCategory>(
-                    context,
-                    size: size,
-                    headingText: "Select Category",
-                    valueKey: "name",
-                    selectedValue: null,
-                    selectedValueID: controller.selectedCategory?.id ?? "",
-                    sheetHeight: 0.5,
-                    filterList: ValueNotifier(controller.categoryList),
-                    onTap: (Object obj) {
-                      controller.selectedCategory = obj as TransactionCategory?;
-                      controller.categoryController.text = controller.selectedCategory?.name ?? "";
-                      Get.back();
-                    },
+                    suffixIcon: Icons.keyboard_arrow_down,
+                    validator: (value) => Validators.required(
+                      value,
+                    ),
+                    onTap: () async => await controller.onDateSelect(context),
                   );
-                },
+                }),
+            CustomTextFieldWidget(
+              controller: controller.descriptionController,
+              upperLabel: "Description".tr,
+              hintValue: "Enter Description".tr,
+              asterisk: "\u{002A}",
+              maxLength: 250,
+              minLines: 3,
+              maxLines: 3,
+              validator: (value) => Validators.required(
+                value,
               ),
-              CustomTextFieldWidget(
-                controller: controller.typeController,
-                isEnabled: false,
-                upperLabel: "Type".tr,
-                asterisk: "\u{002A}",
-                hintValue: "Select Type".tr,
-                prefixText: Padding(
-                  padding: const EdgeInsets.only(left: 12),
-                  child: CustomImageView(
-                    svgPath: AppImages.transactionTypeSvg,
-                    color: AppTheme.greyColor,
-                  ),
-                ),
-                suffixIcon: Icons.keyboard_arrow_down,
-                showStaticBottomShadow: true,
-                validator: (value) => Validators.required(
-                  value,
-                ),
-                onTap: () {
-                  valueSelectionBottomSheet<TransactionType>(
-                    context,
-                    size: size,
-                    headingText: "Select Type",
-                    valueKey: "name",
-                    selectedValue: controller.selectedTransactionType?.name,
-                    sheetHeight: 0.3,
-                    filterList: ValueNotifier(TransactionType.values),
-                    onTap: (Object obj) {
-                      controller.selectedTransactionType = obj as TransactionType?;
-                      controller.typeController.text = controller.selectedTransactionType?.name ?? "";
-                      Get.back();
-                    },
-                  );
-                },
-              ),
-              GetBuilder(
-                  init: controller,
-                  id: "date",
-                  builder: (_) {
-                    return CustomTextFieldWidget(
-                      controller: controller.dateController,
-                      isEnabled: false,
-                      upperLabel: "Select Date".tr,
-                      asterisk: "\u{002A}",
-                      hintValue: "Ex. 01 Jan, 2025".tr,
-                      prefixText: Padding(
-                        padding: const EdgeInsets.only(left: 12),
-                        child: CustomImageView(
-                          svgPath: AppImages.calendarSvg,
-                          color: AppTheme.greyColor,
-                        ),
-                      ),
-                      suffixIcon: Icons.keyboard_arrow_down,
-                      validator: (value) => Validators.required(
-                        value,
-                      ),
-                      onTap: () {},
-                    );
-                  }),
-              CustomTextFieldWidget(
-                controller: controller.descriptionController,
-                upperLabel: "Description".tr,
-                hintValue: "Enter Description".tr,
-                asterisk: "\u{002A}",
-                maxLength: 250,
-                minLines: 3,
-                maxLines: 3,
-                validator: (value) => Validators.required(
-                  value,
-                ),
-                textCapitalization: TextCapitalization.none,
-                type: TextInputType.text,
-                showStaticBottomShadow: true,
-              ),
-            ],
-          ),
+              textCapitalization: TextCapitalization.none,
+              type: TextInputType.text,
+              showStaticBottomShadow: true,
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomButton(
+            margin: getMargin(left: 14, right: 14, top: 18, bottom: 34),
+            borderRadius: 36,
+            text: "Add Transaction".tr,
+            showGradient: true,
+            onTap: () {
+              if (!controller.formKey.currentState!.validate()) {
+                return;
+              }
+              controller.onAddTransaction();
+            },
+          ),
+        ],
       ),
     );
   }
