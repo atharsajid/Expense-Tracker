2fe6d37ec51dc2f00d7807d519454cfa81dd7702
atharsajid <athersajid620@gmail.com>
2025-05-29 00:07:28 +0500

    Added new icons and images.

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
+  //PNG Icons
   static const String appLogo = "assets/icons/logo.png";
   static const String airplaneIcon = "assets/icons/airplane.png";
   static const String bagIcon = "assets/icons/bag.png";
@@ -7,9 +8,16 @@ class AppImages {
   static const String transportIcon = "assets/icons/transport.png";
   static const String cashIcon = "assets/icons/cash.png";
 
+  //SVG Icons
   static const String addIconSvg = "assets/icons/plus.svg";
   static const String gridSquareSvg = "assets/icons/grid_squares.svg";
   static const String barChartSvg = "assets/icons/bar_chart_square.svg";
   static const String transactionTypeSvg = "assets/icons/shield_dollar.svg";
   static const String calendarSvg = "assets/icons/calendar_event.svg";
+  static const String checkSvg = "assets/icons/check.svg";
+  static const String alertCircleSvg = "assets/icons/alert_circle.svg";
+  static const String filterLinesSvg = "assets/icons/filter_lines.svg";
+
+  //Images
+  static const String bgRipplePatternImage = "assets/images/bg_ripple_pattern_trimmed_left.png";
 }
