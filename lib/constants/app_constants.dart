cdb952700edb8700081fa161519452b551349cd3
atharsajid <athersajid620@gmail.com>
2025-05-29 00:07:48 +0500

    Added app constants.

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  ///E.g 01 Jan, 2025
+  static const String dateFormat = 'dd MMM, yyyy';
+  static const String appFont = "Inter";
+}
--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -1,4 +1,7 @@
 class AppStrings {
   static const String appName = 'Expense Tracker';
   static const String addTransaction = 'Add Transaction';
+  static const String transactions = 'Transactions';
+  static const String transactionAddedSuccessfullyMsg = 'Transaction added successfully';
+  static const String someThingWentWrongMsg = 'Some thing went wrong!';
 }
