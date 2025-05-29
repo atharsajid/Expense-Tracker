60d1ce24863aa38a8d3f000788c387880a7cd371
atharsajid <athersajid620@gmail.com>
2025-05-30 00:35:01 +0500

    Create extension to check date between two dates.

--- lib/utilities/extensions.dart/date_time_extensions.dart
+++ lib/utilities/extensions.dart/date_time_extensions.dart
@@ -1,8 +1,41 @@
 import 'package:expense_tracker/constants/app_constants.dart';
 import 'package:intl/intl.dart';
 
-extension DateTimeExtensions on DateTime {
+extension DateTimeExtensions on DateTime? {
   String toFormatDateTime({String format = ''}) {
-    return DateFormat(format.isEmpty ? AppConstants.dateFormat : format).format(this);
+    if (this == null) return "";
+    return DateFormat(format.isEmpty ? AppConstants.dateFormat : format).format(this!);
+  }
+
+  bool? isAfterOrEqualTo(DateTime dateTime) {
+    final date = this;
+    if (date != null) {
+      final isAtSameMomentAs = dateTime.isAtSameMomentAs(date);
+      return isAtSameMomentAs | date.isAfter(dateTime);
+    }
+    return null;
+  }
+
+  bool? isBeforeOrEqualTo(DateTime dateTime) {
+    final date = this;
+    if (date != null) {
+      final isAtSameMomentAs = dateTime.isAtSameMomentAs(date);
+      return isAtSameMomentAs | date.isBefore(dateTime);
+    }
+    return null;
+  }
+
+  bool? isBetween(
+    DateTime? fromDateTime,
+    DateTime? toDateTime,
+  ) {
+    if (fromDateTime == null || toDateTime == null) return null;
+    final date = this;
+    if (date != null) {
+      final isAfter = date.isAfterOrEqualTo(fromDateTime) ?? false;
+      final isBefore = date.isBeforeOrEqualTo(toDateTime) ?? false;
+      return isAfter && isBefore;
+    }
+    return null;
   }
 }
